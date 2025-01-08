e55f664c2a97d10f9ca36ca8322908e01a37b591
Pedro-luiz-melo <pedro.venancio@pdbomdespacho.com.br>
2025-01-08 15:50:09 -0300

    aula2_questao2.py

---
+++ aula2_questao2.py
@@ -0,0 +1,11 @@
+juros = 1.01
+saldo = 500
+print(saldo)
+print(type(saldo))
+saldo = saldo * juros #505 rendimento 1
+print(saldo)
+saldo = saldo * juros #510 rendimento 2
+print(saldo)
+saldo = saldo * juros #515 rendimento 3
+print(saldo)
+print(type(saldo))
